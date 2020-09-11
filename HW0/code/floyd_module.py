95cb3ad69a5c41bf5a7578b06830a1c608005db4
Floyd <36140953+ziangzha@users.noreply.github.com>
2020-09-11 00:25:07 -0500

    Update floyd_module.py

--- HW0/code/floyd_module.py
+++ HW0/code/floyd_module.py
@@ -2,24 +2,26 @@ import numpy as np
 
 """ small changes """
 
-def square_a_number(x):
+def times_by_10(x):
 	"""
-    Dane's first function.
+    Floyd's first function.
     Args:
         x (float): Some number.
     Returns:
-        float: Returns :math:`y=x^2`
+        float: Returns :math:`y=10x`
     """
 
-	return x**2
+	return 10*x
 
-def times_by_3(x):
+def plus_two(x):
 	"""
-    Dane's second function.
+    Floyd's first function.
     Args:
         x (float): Some number.
     Returns:
-        float: Returns :math:`y=3x`
+        float: Returns :math:`y=x+2`
     """
 
-	return 3*x
+	return x+2
+
+
